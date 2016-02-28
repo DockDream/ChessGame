c33b2dab78ba2a5bcf3037e1080d6c975b9aed5a
Sai Patchala <saipatchala@gmail.com>
2016-02-28 09:24:53 -0600

    Initial Pieces Classes

---
+++ src/Game.java
@@ -0,0 +1,47 @@
+
+public class Game {
+	
+	Piece [][] ChessBoard = new Piece[8][8];
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Game FirstGame = new Game();
+		int ClickedRow = 0;
+		int ClickedColumn = 0;
+		
+		FirstGame.InitializeGame();
+		
+		if (ClickedRow<=8 && ClickedRow>=0 && ClickedColumn<=8 && ClickedColumn >=0){
+			
+		}else{
+			System.out.println("ClickedRow and ClickedColumn Values sent are invalid");
+		}
+		
+		if (FirstGame.ChessBoard[ClickedRow][ClickedColumn] == null){
+			//Not a valid Click
+		}
+		
+		
+		
+	}
+	
+	//Starts the Board at Initial Configuration
+	public void InitializeGame(){
+		ChessBoard[0][0] = new Rook();
+		ChessBoard[0][1] = new Rook();
+		ChessBoard[0][2] = new Horse();
+		ChessBoard[0][3] = new Bishop();
+		ChessBoard[0][4] = new Queen();
+		ChessBoard[0][5] = new King();
+		ChessBoard[0][6] = new Bishop();
+		ChessBoard[0][7] = new Horse();
+		
+		for (int i = 0; i < 8; i++){
+			ChessBoard[1][i] = new Pawn();
+		}
+	}
+	
+	
+
+}
---
+++ src/Piece.java
@@ -0,0 +1,16 @@
+
+public abstract class Piece {
+	
+	boolean team;
+	
+	//team = true means white
+	//team = false means black
+	
+	
+	public boolean validmove(){
+		
+		return false;
+	}
+	
+	
+}
