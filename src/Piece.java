9721ee65e9059012d7b63c55d85cb5332d41c616
Sai Patchala <saipatchala@gmail.com>
2016-03-03 19:59:59 -0500

    Updated with common resources

--- src/Piece.java
+++ src/Piece.java
@@ -1,22 +1,40 @@
+import java.util.ArrayList;
+
 
 public abstract class Piece {
 	
 	boolean team;
+	ArrayList<int[]> possibleMoves; //store all possible moves for each piece
+	
 	
 	//team = true means white
 	//team = false means black
 	
 	
-	public boolean ValidMove(){
+	public boolean ValidMove(int destRow, int destColumn){
+		for (int i = 0; i < possibleMoves.size(); i++){
+			if (possibleMoves.get(i)[0] == destRow && possibleMoves.get(i)[1] == destColumn){
+				return true;
+			}
+		}
 		
 		return false;
 	}
 	
-	public ArrayList<Array> PossibleMoves(){
-		
+	public ArrayList<int[]> PossibleMoves(){
+		return null;
+		//Array = [row, column]
 	}
 	
-	public boolean KingCheck(){
+	public boolean KingCheck(Piece[][] currentBoard){
 		
+		for (int i = 0; i < possibleMoves.size(); i++){
+			if (currentBoard[possibleMoves.get(i)[0]][possibleMoves.get(i)[1]] instanceof King
+					&& currentBoard[possibleMoves.get(i)[0]][possibleMoves.get(i)[1]].team == this.team){
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
