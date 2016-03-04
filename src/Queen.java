1b33a6adc383a0589bb6362eee2bef8e8f77bc9e
RyanBurns <ryanb2633@gmail.com>
2016-03-03 18:24:47 -0700

    Updated Queen trial 2

--- src/Queen.java
+++ src/Queen.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
 
 public class Queen extends Piece{
+	
+	public ArrayList<int[]> PossibleMoves(int startRow, int startColumn, Piece[][] currentBoard){
+		return null;
+	}
 	
 	
 }
